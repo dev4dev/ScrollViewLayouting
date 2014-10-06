c03a47b1c5eb44b3870faf520a19c6b137117928
Alex Antonyuk <alex@antonyuk.me>
2014-10-06 20:57:19 +0300

    Initial Commit

---
+++ ScrollViewLayouting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewLayouting
+//
+//  Created by Alex Antonyuk on 10/6/14.
+//  Copyright (c) 2014 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewLayouting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewLayouting
+//
+//  Created by Alex Antonyuk on 10/6/14.
+//  Copyright (c) 2014 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
